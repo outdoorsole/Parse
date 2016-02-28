8cf8d38e47cf2cba7330e70262813fb61ba1a551
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-02-27 18:28:28 -0800

    Initialize Parse ('didFinishLaunchingWithOptions' in App Delegate file.)

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -17,6 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Initialize Parse
+        // Set applicationId and server based on the values in the Heroku settings.
+        // clientKey is not used on Parse open source unless explicitly configured
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "myAppId"
+                configuration.server = "https://myAppName.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
